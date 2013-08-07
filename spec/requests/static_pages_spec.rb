726d73973c9b03899e876f5d4d39f6b32b030282
nitinsaxena0311 <nitinsaxena0311@gmail.com>
2013-08-08 02:05:34 +0530

    Add a title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,9 +9,13 @@ describe "Static pages" do
       expect(page).to have_content('Home')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App")
+    end
+    it "should not have a custom page title" do
+    	visit '/static_pages/home'
+    	expect(page).not_to have_title(' | Home')
     end
   end
 
@@ -22,9 +26,13 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
 
-    it "should have the title 'Help'" do
+    it "should have the base title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App | Help")
+      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App")
+    end
+    it "should not have a custom page title" do
+    	visit '/static_pages/help'
+    	expect(page).not_to have_title(' | Help')
     end
   end
 
@@ -35,9 +43,30 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
 
-    it "should have the title 'About Us'" do
+    it "should have the base about" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App | About Us")
+      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App")
+    end
+    it "should not have a custom page title" do
+    	visit '/static_pages/about'
+    	expect(page).not_to have_title(' | About')
+    end
+  end
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial MicroPost App")
+    end
+
+    it "should not have a custom page title" do
+    	visit '/static_pages/contact'
+    	expect(page).not_to have_title(' | Contact')
     end
   end
 end
